73b6b99135e049546a2ece0f823842e2efc27911
mtan22 <76962221+mtan22@users.noreply.github.com>
2021-08-27 17:37:02 -0700

    Create printing.py

---
+++ labs/lab1/printing/printing.py
@@ -0,0 +1,18 @@
+# Michelle Tan
+# Prof. Turner
+# Section 01
+
+def print_examples():
+   # these variable values below are needed to overcome errors when the print statements are executed
+   n = 2
+   m = 4
+   f = 42.7
+   # place the print calls specified in the lab description below here
+   print("Entered print_examples function.\n")
+   print("n:", n)
+   n=99
+   print("n:", n, "\tm:", m)
+   print("f:", f)
+
+if __name__ == '__main__':
+   print_examples()                 
